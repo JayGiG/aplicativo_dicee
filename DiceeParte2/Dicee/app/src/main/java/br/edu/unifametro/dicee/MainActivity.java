489cb227e91d59baee386c1495fc9bf78e46fb88
Janiele Nogueira <njaniele@yahoo.com>
2020-09-05 17:31:39 -0300

    Projeto criado

---
+++ DiceeParte2/Dicee/app/src/main/java/br/edu/unifametro/dicee/MainActivity.java
@@ -0,0 +1,79 @@
+package br.edu.unifametro.dicee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button roolButton = findViewById(R.id.rollButton);
+
+        final ImageView leftDice  = findViewById(R.id.image_leftDice);
+        final ImageView rightDice  = findViewById(R.id.image_rightDice);
+
+
+        final int
+                [] diceeArray = { //final torna o array constante
+                R.drawable.
+                        dice1,
+                R.drawable.
+                        dice2,
+                R.drawable.
+                        dice3,
+                R.drawable.
+                        dice4,
+                R.drawable.
+                        dice5,
+                R.drawable.
+                        dice6
+        };
+
+        //Evento de sorteio dos dados ao clicar no botão
+        roolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random numberRandom = new Random();
+
+                int number = numberRandom.nextInt(6); //aleatório [
+                leftDice.setImageResource(diceeArray[number]);
+                number = numberRandom.nextInt(6);
+                rightDice.setImageResource(diceeArray[number]);
+            }
+        });
+
+        //Evento que sorteia apenas o dado esquerdo ao clicar nele
+        leftDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random numberRandom = new Random();
+
+                int number = numberRandom.nextInt(6); //aleatório [
+                leftDice.setImageResource(diceeArray[number]);
+
+            }
+        });
+
+        //Evento que sorteia apenas o dado direito ao clicar nele
+        rightDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random numberRandom = new Random();
+
+                int number = numberRandom.nextInt(6); //aleatório [
+                rightDice.setImageResource(diceeArray[number]);
+
+            }
+        });
+
+    }
+}
